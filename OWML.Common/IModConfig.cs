725165e297a3952166bfac002d1aa89a56845379
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-06-03 15:06:09 +0100

    Added more summaries
    - IModConfig
    - IModHelper
    - Removed from ModHelper

--- OWML.Common/IModConfig.cs
+++ OWML.Common/IModConfig.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 
 namespace OWML.Common
 {
+    /// <summary>
+    /// The config file for each mod.
+    /// </summary>
     public interface IModConfig
     {
+        /// <summary>Is the mod enabled?</summary>
         bool Enabled { get; set; }
+
+        /// <summary>Does the mod use VR?</summary>
         bool RequireVR { get; set; }
+
         Dictionary<string, object> Settings { get; set; }
+
+        /// <summary>Get a value from the config file.</summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="key">The name of the setting.</param>
         T GetSettingsValue<T>(string key);
+
+        /// <summary>Set a value in the config file.</summary>
+        /// <param name="key">The name of the setting.</param>
+        /// <param name="value">What value to set it to.</param>
         void SetSettingsValue(string key, object value);
 
         [Obsolete("Use GetSettingsValue instead")]
--- OWML.ModHelper/ModHelper.cs
+++ OWML.ModHelper/ModHelper.cs
@@ -18,20 +18,6 @@ namespace OWML.ModHelper
         public IOwmlConfig OwmlConfig { get; }
         public IModInteraction Interaction { get; }
 
-        /// <summary>
-        /// Create an instance.
-        /// </summary>
-        /// <param name="logger"></param>
-        /// <param name="console"></param>
-        /// <param name="harmonyHelper"></param>
-        /// <param name="events"></param>
-        /// <param name="assets"></param>
-        /// <param name="storage"></param>
-        /// <param name="menus"></param>
-        /// <param name="manifest"></param>
-        /// <param name="config"></param>
-        /// <param name="owmlConfig"></param>
-        /// <param name="interaction"></param>
         public ModHelper(IModLogger logger, IModConsole console, IHarmonyHelper harmonyHelper, IModEvents events, 
             IModAssets assets, IModStorage storage, IModMenus menus, IModManifest manifest, IModConfig config, IOwmlConfig owmlConfig, IModInteraction interaction)
         {
